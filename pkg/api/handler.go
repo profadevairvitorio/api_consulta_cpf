746c84427ea3a4a14a3cbe635dd60efdafaf5bf9
profadevairvitorio <profadevairvitorio@gmail.com>
2023-12-03 22:06:59 -0300

    create handler

---
+++ pkg/api/handler.go
@@ -0,0 +1,48 @@
+// pkg/api/handler.go
+
+package api
+
+import (
+	"encoding/json"
+	"github.com/profadevairvitorio/cpf-lookup/internal/domain/service"
+	"net/http"
+)
+
+type CPFHandler struct {
+	CPFService *service.CPFService
+}
+
+func NewCPFHandler(cpfService *service.CPFService) *CPFHandler {
+	return &CPFHandler{
+		CPFService: cpfService,
+	}
+}
+
+func (h *CPFHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		h.handleCPFQuery(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (h *CPFHandler) handleCPFQuery(w http.ResponseWriter, r *http.Request) {
+	var request CPFRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, "Error reading the request body", http.StatusBadRequest)
+		return
+	}
+
+	response := h.CPFService.QueryCPF(request.CPF)
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Error converting to JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
+}
