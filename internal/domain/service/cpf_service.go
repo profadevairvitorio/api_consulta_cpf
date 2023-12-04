c95dbdca28b893ecd54ab2f205100e971e74743e
profadevairvitorio <profadevairvitorio@gmail.com>
2023-12-03 22:06:47 -0300

    create cpf service

---
+++ internal/domain/service/cpf_service.go
@@ -0,0 +1,17 @@
+// internal/domain/service/cpf_service.go
+
+package service
+
+import (
+	"github.com/profadevairvitorio/cpf-lookup/internal/domain/model"
+)
+
+type CPFService struct{}
+
+func NewCPFService() *CPFService {
+	return &CPFService{}
+}
+
+func (s *CPFService) QueryCPF(cpf string) *model.CPF {
+	return &model.CPF{Value: cpf}
+}
